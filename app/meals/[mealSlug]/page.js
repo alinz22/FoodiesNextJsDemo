8b912a05f8c955c70ed98374284daac6b0b31f0f
Alan Mong <alanmongx@gmail.com>
2024-07-24 22:05:35 -0700

    add page for each meal item along with a not found page for when a meal is not found

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,39 @@
-export default function MealsDetailsPage() {
-  return <h1>Meals Details Page</h1>;
+import Image from "next/image";
+import classes from "./page.module.css";
+import { notFound } from "next/navigation";
+import { getMealById } from "@/lib/meal";
+
+export default function MealsDetailsPage({ params }) {
+  const meal = getMealById(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br>");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            BY <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
