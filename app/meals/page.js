721b4cd498c43c64efb73c0a4831ca2c4d345824
Alan Mong <alanmongx@gmail.com>
2024-07-24 19:10:54 -0700

    implement loading section when fethcing data

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,10 +2,15 @@ import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meal";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
-  const meals = await getMeals()
+async function Meals() {
+  const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +26,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching Meals</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
